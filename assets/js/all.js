d6059b8eaab681764db5a094c43a9d3dc895cb99
“Hsu <hsuyu27@gmail.com>
2021-08-24 15:46:39 +0800

    Update 2021-08-24T07:46:38.432Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,20 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+//swipers
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+}); //Aos
+
+AOS.init();
 //# sourceMappingURL=all.js.map
